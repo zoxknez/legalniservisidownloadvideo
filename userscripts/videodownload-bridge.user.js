5c08c76103b8337fe8b3907cf1f40be5a00f1f7b
zoxknez <zoxknez@hotmail.com>
2026-06-10 02:18:44 +0200

    bump: bump app and userscript versions for new release

--- userscripts/videodownload-bridge.user.js
+++ userscripts/videodownload-bridge.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Video Download Servisi — Bridge (Sniffer + Sesije)
 // @namespace    https://github.com/videodownloadservisi
-// @version      2.0.0
+// @version      2.1.0
 // @description  Automatski šalje sesije i snifuje MPD/license URL-ove u lokalnu aplikaciju.
 // @author       Video Download Servisi
 // @match        *://*.max.com/*
